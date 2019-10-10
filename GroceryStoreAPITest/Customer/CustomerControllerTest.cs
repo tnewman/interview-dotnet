cfed2c79be0ca7139993b2b88ab5f8f46036f1d6
Thomas Newman <tnewman@users.noreply.github.com>
2019-10-09 21:03:39 -0400

    Setup test skeleton

---
+++ GroceryStoreAPITest/Customer/CustomerControllerTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GroceryStoreAPITest.Customer
+{
+    class CustomerControllerTest
+    {
+    }
+}
---
+++ GroceryStoreAPITest/Order/OrderRepositoryTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GroceryStoreAPITest.Order
+{
+    class OrderRepositoryTest
+    {
+    }
+}
