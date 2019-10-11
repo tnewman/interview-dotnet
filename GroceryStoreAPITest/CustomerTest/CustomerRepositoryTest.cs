add5a3189fb5c9640cbeb2b2b6462fef26cf23c9
Thomas Newman <tnewman@users.noreply.github.com>
2019-10-10 23:50:31 -0400

    Add customer repository tests

--- GroceryStoreAPITest/CustomerTest/CustomerRepositoryTest.cs
+++ GroceryStoreAPITest/CustomerTest/CustomerRepositoryTest.cs
@@ -25,31 +25,116 @@ namespace GroceryStoreAPITest.CustomerTest
         [TestMethod]
         public void TestList()
         {
+            IEnumerable<Customer> customersToRetrieve = new List<Customer>()
+            {
+                new Customer()
+                {
+                    Id = 1
+                }
+            };
 
+            JSONData jsonData = new JSONData()
+            {
+                Customers = customersToRetrieve
+            };
+
+            this.jsonDatabase.Setup(jd => jd.loadJSONData()).Returns(jsonData);
+
+            IEnumerable<Customer> retrievedCustomers = this.customerRepository.List();
+
+            Assert.AreEqual(customersToRetrieve.Count(), retrievedCustomers.Count());
+            Assert.AreEqual(customersToRetrieve.First().Id, retrievedCustomers.First().Id);
         }
 
         [TestMethod]
         public void TestGetWithId()
         {
+            IEnumerable<Customer> customersToRetrieve = new List<Customer>()
+            {
+                new Customer()
+                {
+                    Id = 1
+                },
+                new Customer()
+                {
+                    Id = 2
+                }
+            };
 
+            JSONData jsonData = new JSONData()
+            {
+                Customers = customersToRetrieve
+            };
+
+            this.jsonDatabase.Setup(jd => jd.loadJSONData()).Returns(jsonData);
+
+            Customer retrievedCustomer = this.customerRepository.Get(2);
+
+            Assert.AreEqual(2, retrievedCustomer.Id);
         }
 
         [TestMethod]
         public void TestGetWithMissingId()
         {
+            IEnumerable<Customer> customersToRetrieve = new List<Customer>();
 
+            JSONData jsonData = new JSONData()
+            {
+                Customers = customersToRetrieve
+            };
+
+            this.jsonDatabase.Setup(jd => jd.loadJSONData()).Returns(jsonData);
+
+            Customer retrievedCustomer = this.customerRepository.Get(2);
+
+            Assert.IsNull(retrievedCustomer);
         }
 
         [TestMethod]
         public void TestSaveInsert()
         {
+            JSONData jsonData = new JSONData();
 
+            this.jsonDatabase.Setup(jd => jd.loadJSONData()).Returns(jsonData);
+
+            Customer customerToSave = new Customer()
+            {
+                Id = 1
+            };
+
+            Customer savedCustomer = this.customerRepository.Save(customerToSave);
+
+            Assert.AreEqual(customerToSave.Id, savedCustomer.Id);
+            this.jsonDatabase.Verify(jd => jd.saveJSONData(jsonData));
         }
 
         [TestMethod]
         public void TestSaveOverwrite()
         {
+            JSONData jsonData = new JSONData()
+            {
+                Customers = new List<Customer>()
+                {
+                    new Customer()
+                    {
+                        Id = 1,
+                        Name = "Test"
+                    }
+                }
+            };
 
+            this.jsonDatabase.Setup(jd => jd.loadJSONData()).Returns(jsonData);
+
+            Customer customerToSave = new Customer()
+            {
+                Id = 1,
+                Name = "Test2"
+            };
+
+            Customer savedCustomer = this.customerRepository.Save(customerToSave);
+
+            Assert.AreEqual(customerToSave.Id, savedCustomer.Id);
+            this.jsonDatabase.Verify(jd => jd.saveJSONData(jsonData));
         }
     }
 }
